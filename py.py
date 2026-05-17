4df8f922bceda9c46221826be66ca9e33cb43cb8
Nguyen Duc An <anamanh310@gmail.com>
2026-05-17 21:43:43 +0700

    Update Selenium YouTube example to latest code

--- py.py
+++ py.py
@@ -4,3 +4,14 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+opts = Options()
+opts.add_argument("--start-maximized")
+# Uncomment to run without opening a browser window:
+# opts.add_argument("--headless=new")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+driver.get("https://www.youtube.com")
+
+time.sleep(10)
+driver.quit()
+
